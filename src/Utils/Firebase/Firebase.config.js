5037ff4a10477c304d04e1a5ce11c937a5007fca
Rakib500538 <rakib2020.tkg@gmail.com>
2023-05-03 22:15:26 +0600

    .env.local file add and secret code set

--- src/Utils/Firebase/Firebase.config.js
+++ src/Utils/Firebase/Firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDfLl-VtcOz1jaiWq_AFXxnoyQCOXGPJPU",
-  authDomain: "larosa-resturant.firebaseapp.com",
-  projectId: "larosa-resturant",
-  storageBucket: "larosa-resturant.appspot.com",
-  messagingSenderId: "1065467346527",
-  appId: "1:1065467346527:web:7566b572a0410ef863a5ea"
+  apiKey:import.meta.env.VITE_APIKEY ,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN ,
+  projectId:import.meta.env.VITE_PROJECTID ,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET ,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID ,
+  appId:import.meta.env.VITE_APPID 
 };
 
 // Initialize Firebase
